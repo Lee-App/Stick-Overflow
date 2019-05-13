cb6b9b968720fd2dabd07ac685117c967fa9908c
Lee-App <kli4574@naver.com>
2019-05-13 13:02:29 +0900

    Add models

--- stickoverflow/models.py
+++ stickoverflow/models.py
@@ -2,6 +2,38 @@ from django.db import models
 
 # Create your models here.
 
+# User Database
+class User(models.Model):
+    user_id = models.CharField(max_length = 50, help_text = 'Enter User ID')
+    password = models.CharField(max_length = 50, help_text = 'Enter password')
+    user_name = models.CharField(max_length = 50, help_text = 'Enter user name')
+    email = models.CharField(max_length = 100, help_text = 'Enter email')
+    department = models.CharField(max_length = 50, help_text = 'Enter department')
+
+    input_id = models.CharField(max_length = 50)
+    input_ip = models.CharField(max_length = 15)
+    input_data = models.DateTimeField()
+
+    update_id = models.CharField(max_length = 50)
+    update_ip = models.CharField(max_length = 15)
+    update_data = models.DateTimeField()
+
+# FILE Database
+class File(models.Model):
+    user_id = models.CharField(max_length = 50)
+    file_no = models.IntegerField()
+    file_name = models.CharField(max_length = 260)
+    file_path = models.CharField(max_length = 260)
+    file_description = models.CharField(max_length = 200)
+
+# RESULT Database
+class Result(models.Model):
+    user_id = models.CharField(max_length = 50, help_text = 'Enter User ID')
+    file_no = models.IntegerField()
+    result_no = models.IntegerField()
+    analysis_code = models.IntegerField()
+    save_code = models.IntegerField()
+
 # /DB 테이블 만들어 보려고 시도한 흔적임.
 
 # class User(models.Model):
@@ -21,4 +53,3 @@ from django.db import models
 # 	db_result_fid = models.IntegerField(default=0) # PK, FK
 
 # DB 테이블 만들어 보려고 시도한 흔적임./
-
